899437c08976811815333bf069eb126fa54d4661
Roy, Niladri <callniladri.pro@gmail.com>
2021-02-12 15:52:30 +0530

    aliasing

--- README.md
+++ README.md
@@ -15,3 +15,5 @@ My tests are in the folder `cypress/integration/mytests`
 
 ## Selecting Elements
 Element tags, names, CSS, id are bad (increasing better, but still bad) as any refactoring or style change can have an impact. Recommended way is to use `data-*` attribute. Some testing frameworks use `data-test-id`, while cypress recommends `data-cy`. I recommend `data-test-id` or something generic so that we wont have framework dependent nomenclature (relevant in the event we have to switch it in future).
+
+Use aliasing for better readability of tests.
---
+++ cypress/integration/mytests/aliasing.spec.js
@@ -0,0 +1,48 @@
+describe('textbox with max characters', () => {
+    it('displays appropriate remaining char count', () => {
+        cy.visit('http://localhost:3000/example-3');
+
+        cy.get('[data-cy="last-name-chars-left-count"]')
+            .as('charsLeftSpan');
+        cy.get('[data-cy="input-last-name"]')
+            .as('lastNameInput');
+
+        cy.get('@charsLeftSpan')
+            .invoke('text')
+            .should('equal', '15');
+
+        cy.get('@lastNameInput')
+            .type('hello');
+
+        cy.get('@charsLeftSpan')
+            .invoke('text')
+            .should('equal', '10');
+
+        cy.get('@lastNameInput')
+            .type(' my friend');
+
+        cy.get('@charsLeftSpan')
+            .invoke('text')
+            .should('equal', '0');
+    })
+
+    it('prevents user from typing more characters than maxlength', () => {
+        cy.visit('http://localhost:3000/example-3');
+        const inputStr = 'some long string that should not fit';
+
+        cy.get('[data-cy="last-name-chars-left-count"]')
+            .as('charsLeftSpan');
+        cy.get('[data-cy="input-last-name"]')
+            .as('lastNameInput');
+
+        cy.get('@lastNameInput')
+            .type(inputStr);
+
+        cy.get('@lastNameInput')
+            .should('have.attr', 'value', inputStr.substr(0, 15));
+
+        cy.get('@charsLeftSpan')
+            .invoke('text')
+            .should('equal', '0');
+    })
+})
